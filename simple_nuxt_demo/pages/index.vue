e63dd29e15bad8039863222627f2c5aec17f62fd
gongpu <gongpu@lambdax.cn>
2017-11-02 11:06:59 +0800

    add source codes

---
+++ simple_nuxt_demo/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<h1>{{foo}}</h1>
+</template>
+
+<script>
+	import axios from 'axios'
+	
+	export default {
+		 asyncData () {
+			var instance = axios.create({
+  				baseURL: 'http://127.0.0.1:8023/api',
+  				timeout: 1000,
+			});
+			instance.post('/signup',{
+                name: 'wangggongpu',
+                password: '12345678',
+                email: '1sds@qq.com'
+            }).then(function (response) {
+				
+                console.log(response)
+            }).catch(function (error) {
+               
+				console.log(error)
+            })
+    		return { foo: 'success' }
+  		}
+		
+	}
+
+</script>
